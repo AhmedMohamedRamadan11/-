6d2d4856f8e1d27899775ba131a19b1c46f4daac
AhmedMohamedRamadan11 <ahmedmramadan332001@gmail.com>
2022-12-24 01:19:42 +0200

    file added

---
+++ القران الكريم/Form1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace القران_الكريم
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void axWindowsMediaPlayer1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            wmp.URL = ("https://server11.mp3quran.net/yasser/001.mp3");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            wmp.URL = ("https://server11.mp3quran.net/yasser/002.mp3");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            wmp.URL = ("https://server11.mp3quran.net/yasser/003.mp3");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            wmp.URL = ("https://server11.mp3quran.net/yasser/004.mp3");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            wmp.URL = ("https://server11.mp3quran.net/yasser/005.mp3");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            wmp.URL = ("https://server11.mp3quran.net/yasser/006.mp3");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            wmp.URL = ("https://server11.mp3quran.net/yasser/007.mp3");
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            wmp.URL = ("https://server11.mp3quran.net/yasser/008.mp3");
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            wmp.URL = ("https://server11.mp3quran.net/yasser/009.mp3");
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            wmp.URL = ("https://server11.mp3quran.net/yasser/010.mp3");
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            wmp.URL = ("https://server11.mp3quran.net/yasser/011.mp3");
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            wmp.URL = ("https://server11.mp3quran.net/yasser/012.mp3");
+
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            wmp.URL = ("https://server11.mp3quran.net/yasser/013.mp3");
+
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            wmp.URL = ("https://server11.mp3quran.net/yasser/014.mp3");
+
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            wmp.URL = ("https://server11.mp3quran.net/yasser/015.mp3");
+
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            wmp.URL = ("https://server11.mp3quran.net/yasser/016.mp3");
+
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            wmp.URL = ("https://server11.mp3quran.net/yasser/017.mp3");
+
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            wmp.URL = ("https://server11.mp3quran.net/yasser/018.mp3");
+
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            wmp.URL = ("https://server11.mp3quran.net/yasser/019.mp3");
+
+        }
+
+        private void button20_Click(object sender, EventArgs e)
+        {
+            wmp.URL = ("https://server11.mp3quran.net/yasser/020.mp3");
+
+        }
+
+        private void button21_Click(object sender, EventArgs e)
+        {
+            wmp.URL = ("https://server11.mp3quran.net/yasser/021.mp3");
+
+        }
+
+        private void button22_Click(object sender, EventArgs e)
+        {
+            wmp.URL = ("https://server11.mp3quran.net/yasser/022.mp3");
+
+        }
+
+        private void button23_Click(object sender, EventArgs e)
+        {
+            wmp.URL = ("https://server11.mp3quran.net/yasser/023.mp3");
+
+        }
+
+        private void button24_Click(object sender, EventArgs e)
+        {
+            wmp.URL = ("https://server11.mp3quran.net/yasser/024.mp3");
+
+        }
+
+        private void button25_Click(object sender, EventArgs e)
+        {
+            wmp.URL = ("https://server11.mp3quran.net/yasser/025.mp3");
+
+        }
+
+        private void button26_Click(object sender, EventArgs e)
+        {
+            wmp.URL = ("https://server11.mp3quran.net/yasser/026.mp3");
+
+        }
+
+        private void button27_Click(object sender, EventArgs e)
+        {
+            wmp.URL = ("https://server11.mp3quran.net/yasser/027.mp3");
+
+        }
+
+        private void button28_Click(object sender, EventArgs e)
+        {
+            wmp.URL = ("https://server11.mp3quran.net/yasser/028.mp3");
+
+        }
+
+        private void button29_Click(object sender, EventArgs e)
+        {
+            wmp.URL = ("https://server11.mp3quran.net/yasser/029.mp3");
+
+        }
+
+        private void button30_Click(object sender, EventArgs e)
+        {
+            wmp.URL = ("https://server11.mp3quran.net/yasser/030.mp3");
+
+        }
+
+        private void button31_Click(object sender, EventArgs e)
+        {
+            wmp.URL = ("https://server11.mp3quran.net/yasser/031.mp3");
+
+        }
+
+        private void button32_Click(object sender, EventArgs e)
+        {
+            wmp.URL = ("https://server11.mp3quran.net/yasser/032.mp3");
+
+        }
+
+        private void button33_Click(object sender, EventArgs e)
+        {
+            wmp.URL = ("https://server11.mp3quran.net/yasser/033.mp3");
+
+        }
+
+        private void button34_Click(object sender, EventArgs e)
+        {
+            wmp.URL = ("https://server11.mp3quran.net/yasser/034.mp3");
+
+        }
+
+        private void button35_Click(object sender, EventArgs e)
+        {
+            wmp.URL = ("https://server11.mp3quran.net/yasser/035.mp3");
+        }
+
+        private void wmp_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
